f231597c2b5c2297eb8122adda0f3d619eb8c45f
V <vlad@macmini.local>
2017-02-19 10:29:11 -0800

    misc.

--- Tip/AppDelegate.swift
+++ Tip/AppDelegate.swift
@@ -12,7 +12,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    var beforeDate: NSDate?, afterDate: NSDate?
+    var didEnterBackgroundDate: NSDate?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -27,18 +27,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-        beforeDate = NSDate()
+        didEnterBackgroundDate = NSDate()
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-        afterDate = NSDate()
-        beforeDate = beforeDate?.addingTimeInterval(600)
-        if afterDate?.compare(beforeDate as! Date) == ComparisonResult.orderedDescending {
-            let nav = self.window?.rootViewController as! UINavigationController;
-            let vc = nav.topViewController as! ViewController
-            vc.billField.text = "0.00"
-            vc.calculateTip(AnyClass.self)
+        if var beforeDate = didEnterBackgroundDate {
+            let afterDate = NSDate()
+            beforeDate = beforeDate.addingTimeInterval(600)
+            if afterDate.compare(beforeDate as Date) == ComparisonResult.orderedDescending {
+                let nav = self.window?.rootViewController as! UINavigationController;
+                let vc = nav.topViewController as! ViewController
+                vc.billField.text = "0.00"
+                vc.calculateTip(AnyClass.self)
+            }
         }
     }
 
